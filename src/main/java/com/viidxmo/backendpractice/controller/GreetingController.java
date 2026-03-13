14e112c874bd2290f00a2f849c934d7cb537ad1e
viidxmo <viidxmo@proton.me>
2026-03-13 21:46:53 +0300

    added greeting post endpoint

--- src/main/java/com/viidxmo/backendpractice/controller/GreetingController.java
+++ src/main/java/com/viidxmo/backendpractice/controller/GreetingController.java
@@ -1,4 +1,28 @@
 package com.viidxmo.backendpractice.controller;
 
+import com.viidxmo.backendpractice.dto.GreetingRequest;
+import com.viidxmo.backendpractice.dto.GreetingResponse;
+import com.viidxmo.backendpractice.service.GreetingService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class GreetingController {
+
+    private final GreetingService greetingService;
+
+
+    public GreetingController(GreetingService greetingService){
+        this.greetingService = greetingService;
+    }
+
+    @PostMapping("/greet")
+    public GreetingResponse greet(@Valid @RequestBody GreetingRequest request){
+        String response = greetingService.generateGreeting(request.name());
+        return new GreetingResponse(response);
+    }
 }
