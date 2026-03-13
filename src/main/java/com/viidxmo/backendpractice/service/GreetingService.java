8f7f602587fca49f684753272b13cd6c0a0fe7c4
viidxmo <viidxmo@proton.me>
2026-03-13 21:45:38 +0300

    added greeting service

--- src/main/java/com/viidxmo/backendpractice/service/GreetingService.java
+++ src/main/java/com/viidxmo/backendpractice/service/GreetingService.java
@@ -1,4 +1,10 @@
 package com.viidxmo.backendpractice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingService {
+    public String generateGreeting(String name){
+        return "Hello, " + name;
+    }
 }
